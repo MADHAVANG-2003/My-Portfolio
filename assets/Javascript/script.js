066a7242f8e363cb68d83476389384f8f7abdb00
MADHAVANG-2003 <madhavangopi2003@gmail.com>
2024-12-19 18:48:16 +0530

    New Update

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,42 +1,6 @@
 const navbar = document.getElementById("navbar");
 window.addEventListener("scroll",()=>{
-  navbar.classList.toggle("sticky",window.scrollY > 180)
-});
-let posX = 0,
-  posY = 0;
-
-let mouseX = 0,
-  mouseY = 0;
-
-gsap.to(".cursor", {
-  duration: 0.018,
-  repeat: -1,
-  onRepeat: function () {
-    posX += (mouseX - posX) / 8;
-    posY += (mouseY - posY) / 8;
-
-    gsap.set(".cursor", {
-      css: {
-        left: posX - 15,
-        top: posY -10
-      }
-    });
-  }
-});
-
-document.addEventListener("mousemove", (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-});
-
-gsap.set(".ball", {xPercent: -50, yPercent: -50});
-
-let xTo = gsap.quickTo(".ball", "x", {duration: 0.6, ease: "power3"}),
-    yTo = gsap.quickTo(".ball", "y", {duration: 0.6, ease: "power3"});
-
-window.addEventListener("mousemove", e => {
-  xTo(e.clientX);
-  yTo(e.clientY);
+  navbar.classList.toggle("sticky",window.scrollY > 80)
 });
 
 const btn = document.getElementById("btn");
@@ -76,5 +40,19 @@ buttons.forEach(button => {
         });
     });
 });
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    buttons.forEach(btn => btn.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
 
-
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      navLinks.forEach(link => link.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
